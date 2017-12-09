28090591be18adca7a92fb4a3322e41b8734f8ce
liuqionghui <511895454@qq.com>
2017-12-09 22:40:52 +0800

    提交工程test

---
+++ test/src/test/Test1.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.io.File;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("这是Test1");
+	}
+
+}
---
+++ test/src/test/Test2.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Test2 {
+	public static void main(String[] args) {
+		System.out.println("这是Test2");
+	}
+}
